7f15de73d147b9884c81c3b8d71e218e86e32308
tstieven <thomas.stievenard@orange.fr>
2017-12-14 14:02:30 -0500

    create ServerTest and ClientTest

---
+++ ClientTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Engine;
+
+public class ClientTest : GameComponent,ILogicComponent
+{
+    public void Start()
+	{
+        ClientComponent client = new ClientComponent("193.162.....",1234);
+        client.Demo();
+	}
+    public void Update()
+    {
+
+    }
+}
---
+++ ServerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Engine;
+
+public class ServerTest : GameComponent,ILogicComponent
+{
+    public void Start()
+	{
+       ServerComponent client = new ServerComponent(1234);
+        client.Demo();
+	}
+    public void Update()
+    {
+
+    }
+}
