0f089f8bb6ab1c0982368ad34c5813c9c5f4237b
tstieven <thomas.stievenard@orange.fr>
2017-12-14 14:05:38 -0500

    test

--- ServerTest.cs
+++ ServerTest.cs
@@ -5,7 +5,7 @@ public class ServerTest : GameComponent,ILogicComponent
 {
     public void Start()
 	{
-       ServerComponent client = new ServerComponent(1234);
+       ServerComponent client = new ServerComponent(1233);
         client.Demo();
 	}
     public void Update()
